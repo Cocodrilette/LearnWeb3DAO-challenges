d7ec82ecd9387e920db53365d2a5142b1745efde
cocodrilette <cocodrilette@gmail.com>
2022-12-05 12:57:34 -0500

    NFT collection project done

---
+++ sophomore/NFTCollection/frontend/constants/index.js
@@ -0,0 +1,5 @@
+import cryptoDevsContract from "./CryptoDevs.json";
+
+export const abi = cryptoDevsContract.abi;
+export const NFT_CONTRACT_ADDRESS =
+  "0x695CC319CEa784CeA1BF56DBAbfc37C67cF2f3c9";
